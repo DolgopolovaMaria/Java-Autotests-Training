96d3c8bd9c764ad50143c372bea96ace8a601b78
mariya.dolgopolova <dolgopolovamariia@gmail.com>
2022-05-04 20:41:14 +0500

    added github test

---
+++ src/test/java/SimpleTests/Tests/GithubTests.java
@@ -0,0 +1,39 @@
+package SimpleTests.Tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.withTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GithubTests {
+
+    @BeforeAll
+    static void setUp(){
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+    void wikiSoftAssertionsTest(){
+        open("/selenide/selenide");
+
+        $("#wiki-tab").click();
+        $(byTagAndText("h1","Welcome to the selenide wiki!")).shouldBe(visible);
+
+        $("#user-content-chapters").parent().sibling(0)
+                .$$("li").filterBy(text("Soft assertions")).first().$("a").click();
+
+        $(byTagAndText("h1", "SoftAssertions")).shouldBe(visible);
+
+        $("#user-content-3-using-junit5-extend-test-class").parent().sibling(0)
+                .$("pre").shouldBe(visible);
+    }
+
+
+}
