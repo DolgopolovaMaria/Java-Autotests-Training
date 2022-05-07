f7b56c441a059ec4f55625d7dbca953f168f58de
Maria Dolgopolova <dolgopolovamariia@gmail.com>
2022-05-07 14:37:17 +0300

    added config

--- src/test/java/PropertiesTest/OwnerTests.java
+++ src/test/java/PropertiesTest/OwnerTests.java
@@ -1,4 +1,19 @@
 package PropertiesTest;
 
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
 public class OwnerTests {
+    CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+    @Tag("owner")
+    @Test
+    void loginTest(){
+        String login = config.login();
+        String password = config.password();
+
+        System.out.println("Login: " + login);
+        System.out.println("Password: " + password);
+    }
 }
